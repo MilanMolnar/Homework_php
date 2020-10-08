6e21c0462c57108e3dfe4a27794f3f7769cbe77d
MilanMolnar <54602124+MilanMolnar@users.noreply.github.com>
2020-10-08 02:06:16 -0700

    first exercise done, there some workaround that I will check later

--- interview_homework/exercise_1/index.php
+++ interview_homework/exercise_1/index.php
@@ -1,1 +1,12 @@
 <?php
+
+function processString($text){
+    //gets everything except for "ő" and "ű"
+    $unaccented =  preg_replace("/&([a-z])[a-z]+;/i", "$1", htmlentities($text));
+    $search = array("ő", "Ő", "ű", "Ű");
+    $replace = array("o", "O", "u", "U");
+    //replaced all accented characters
+    $unaccented = str_replace($search, $replace, $unaccented);
+    //replace all special characters with dashes
+    return preg_replace('/[^A-Za-z0-9\-]/', '-', $unaccented);
+}
