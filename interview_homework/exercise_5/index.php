a47fae5960f8e4745c44f0d6cd738c3093dd57a1
MilanMolnar <54602124+MilanMolnar@users.noreply.github.com>
2020-10-08 06:17:32 -0700

    fifth exercise done, needed a bit more inspiration, but works as intended.

---
+++ interview_homework/exercise_5/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <h2 id="title">Falatozz.hu Homework</h2>
+
+        <div class="row">
+          <div class="column" style="background-color:#e6fff0;">
+            <h2>Column 1</h2>
+            <p>Visible for all devices</p>
+          </div>
+
+          <div class="column" style="background-color:#cbd7fc;">
+            <h2>Column 2</h2>
+            <p>Visible for all devices</p>
+          </div>
+
+          <div class="column" style="background-color:#ffe9c6;">
+            <h2>Column 3</h2>
+            <p>Visible for all devices</p>
+          </div>
+
+          <div class="column" id="mobile_exclude" style="background-color:#d6c5ff;">
+            <h2>Column 4</h2>
+            <p>Should not be visable on mobile</p>
+          </div>
+
+        </div>
+    </body>
+</html>
