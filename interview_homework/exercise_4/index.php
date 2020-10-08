abebd7a88263c712e0285f2b0b88c856814ec456
MilanMolnar <54602124+MilanMolnar@users.noreply.github.com>
2020-10-08 05:40:12 -0700

    fourth exercise is done, might work on the design later. good enough for now.

---
+++ interview_homework/exercise_4/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <ul id="drop-nav">
+        <li><a href="#">Home_Menu</a>
+        <li><a href="#">Menu_01</a>
+            <ul>
+                <li><a href="#">item_01</a></li>
+                <li><a href="#">item_02</a></li>
+                <li><a href="#">item_03</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Menu_02</a>
+            <ul>
+                <li><a href="#">item_01</a></li>
+                <li><a href="#">item_02</a></li>
+                <li><a href="#">item_03</a></li>
+                <li><a href="#">item_04</a></li>
+            </ul>
+        </li>
+    </ul>
+</body>
+</html>
