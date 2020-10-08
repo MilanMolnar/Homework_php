a198f416c32c5f774098e3619bc8cd9074c095bf
MilanMolnar <54602124+MilanMolnar@users.noreply.github.com>
2020-10-08 05:02:54 -0700

    third (optional) exercise done, there are some warnings might check later

---
+++ interview_homework/exercise_3/upload.php
@@ -0,0 +1,24 @@
+<?php
+
+$uploaddir = 'uploads/';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+$file = $_FILES['userfile']['tmp_name'];
+
+echo "<p>";
+if (exif_imagetype($file) == IMAGETYPE_JPEG || exif_imagetype($file) == IMAGETYPE_PNG){
+    if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+        echo "File is valid, and was successfully uploaded.\n";
+    } else {
+        echo "Upload failed";
+    }
+}else{
+    echo "The file uploaded, is not JPEG nor PNG";
+}
+
+echo "</p>";
+echo '<pre>';
+echo 'file info: ';
+print_r($_FILES);
+print "</pre>";
+
+?>
